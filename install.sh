a2fd6a6d5ffde09d44c73776ed7fbf1b43b616a1
Chemas <59790738+SyedaliHOH@users.noreply.github.com>
2023-04-13 08:18:00 +0500

    Making it simple

--- install.sh
+++ install.sh
@@ -10,40 +10,15 @@ echo "APT-GET UPDATE"
 echo "========================"
 sudo apt-get update
 
-echo "========================"
-echo "Installing dnspython"
-echo "========================"
-sudo apt-get install -y python3-dnspython
+packages="python3-dnspython python3-bs4 python3-termcolor python3-requests python3-dns python3-jsbeautifier python3-urllib3 nmap gobuster curl whatweb dirsearch sslscan enum4linux rpcbind nfs-common"
 
-echo "========================"
-echo "Installing BeautifulSoup"
-echo "========================"
-sudo apt-get install -y python3-bs4
-
-echo "========================"
-echo "Installing termcolor"
-echo "========================"
-sudo apt-get install -y python3-termcolor
-
-echo "========================"
-echo "Installing requests"
-echo "========================"
-sudo apt-get install -y python3-requests
-
-echo "========================"
-echo "Installing dns"
-echo "========================"
-sudo apt-get install -y python3-dns
-
-echo "========================"
-echo "Installing jsbeautifier"
-echo "========================"
-sudo apt-get install -y python3-jsbeautifier
-
-echo "========================"
-echo "Installing urllib3"
-echo "========================"
-sudo apt-get install -y python3-urllib3
+for pkg in $packages
+do
+    echo "========================"
+    echo "Installing $pkg"
+    echo "========================"
+    sudo apt-get install -y $pkg
+done
 
 echo "========================"
 echo "Installing ssh-audit"
@@ -54,55 +29,4 @@ python3 setup.py install
 cd ../..
 
 echo "==========================="
-echo "Installing nmap"
-echo "==========================="
-
-sudo apt-get install nmap -y
-
-echo "==========================="
-echo "Installing gobuster"
-echo "==========================="
-
-sudo apt-get install gobuster -y
-
-echo "==========================="
-echo "Installing curl"
-echo "==========================="
-
-sudo apt-get install curl -y
-
-echo "==========================="
-echo "Installing whatweb"
-echo "==========================="
-
-sudo apt-get install whatweb -y
-
-echo "==========================="
-echo "Installing dirsearch"
-echo "==========================="
-
-sudo apt-get install dirsearch -y
-
-echo "==========================="
-echo "Installing sslscan"
-echo "==========================="
-
-sudo apt-get install sslscan -y
-
-echo "==========================="
-echo "Installing enum4linux"
-echo "==========================="
-
-sudo apt-get install enum4linux -y
-
-echo "==========================="
-echo "Installing rpcinfo"
-echo "==========================="
-
-sudo apt-get install rpcbind -y
-
-echo "==========================="
-echo "Installing showmount"
-echo "==========================="
-
-sudo apt-get install nfs-common -y
+echo "All Tools installed successfully"
